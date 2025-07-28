b1fced2fd29935dcd01adf0f5eeaf9b0d6c6f905
Prashant Adhikari <prashantadhikareeey@gmail.com>
2025-07-28 23:31:00 +0545

    update: dynamic projects done

--- src/app/dashboard/projects/[projectId]/page.tsx
+++ src/app/dashboard/projects/[projectId]/page.tsx
@@ -1,6 +1,7 @@
+// app/dashboard/projects/[projectId]/page.tsx
 import ClientPage from "./ClientPage";
 
-export default function Page({ params }: { params: { projectId: string } }) {
-  const {projectId} =  params ;
+export default async function Page({ params }: { params: Promise<{ projectId: string }> }) {
+  const { projectId } = await params;
   return <ClientPage projectId={projectId} />;
 }
